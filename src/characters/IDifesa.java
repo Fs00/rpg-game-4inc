62a900f47ffabbeac41544e8311ffe1d4c901f12
Fs00 <francescosaltori@gmail.com>
2018-01-22 16:18:09 +0100

    Partial implementation of GestionePartita See #13 for details and discussion

--- src/characters/IDifesa.java
+++ src/characters/IDifesa.java
@@ -1,5 +1,8 @@
 package ittbuonarroti.rpggame.characters;
 
+/**
+ * Interfaccia che devono implementare tutti i personaggi in gradi di mitigare i colpi
+ */
 public interface IDifesa {
     /**
      * Il personaggio perde un turno per poi subire danni dimezzati nel turno successivo
--- src/engine/GestionePartita.java
+++ src/engine/GestionePartita.java
@@ -4,37 +4,59 @@ import ittbuonarroti.rpggame.characters.Personaggio;
 
 public class GestionePartita {
 
-    private Personaggio player, enemy;
-    private int turno, turno_cont; //indice turno e contatore turni
-    private boolean run;
+    private Personaggio[] giocatori;    // array contenente i giocatori
+    private int[] ordineGiocatori;      // array contenente gli indici dei giocatori nell'ordine in cui attaccheranno nel turno corrente
+    private int turnoAttuale;           // contatore turno attuale
+    private boolean run;                // ?
 
-    public GestionePartita(Personaggio inPlayer, Personaggio inEnemy) {
-        this.player = inPlayer;
-        this.enemy = inEnemy;
-        this.turno_cont = 0;
-        this.run = false;
-
-        if(inPlayer.getVelocita() > inEnemy.getVelocita())
-            turno = 1; //TURNO GIOCATORE
-        else
-            turno = 2;
+    public GestionePartita(Personaggio[] giocatori) {
+        this.giocatori = giocatori;
+        ordineGiocatori = new int[giocatori.length];
+        turnoAttuale = 1;
     }
 
-    public GestionePartita(Personaggio[] inPlayers, Personaggio[] inEnemies) {
-        //TODO LOTTA MULTIPLA
+    public static void stampaMessaggio(String messaggio) {
+        System.out.println(messaggio);
+        // TODO: quando ci sarà la GUI, adattare questo metodo
+    }
+
+    /**
+     * Effettua le dovute preparazioni per il turno di battaglia successivo
+     */
+    public void turnoSuccessivo() {
+        turnoAttuale++;
+
+        // Riempi l'array degli indici dei giocatori con i loro indici
+        for (int i = 0; i < giocatori.length; i++) {
+            ordineGiocatori[i] = i;
+        }
+
+        // Riordina gli indici in base alla velocità dei giocatori (l'indice del giocatore più veloce è il primo)
+        for (int i = 0; i < ordineGiocatori.length - 1; i++) {
+            for (int j = 0; j < ordineGiocatori.length; i++) {
+                if (giocatori[i].getVelocita() < giocatori[j].getVelocita()) {
+                    int swap = ordineGiocatori[i];
+                    ordineGiocatori[i] = ordineGiocatori[j];
+                    ordineGiocatori[j] = swap;
+                }
+            }
+        }
+    }
+
+    public void faiMossa(int indiceGiocatore, int codiceMossa) {
+        // TODO
+    }
+
+    public int[] getOrdineGiocatori() {
+        return ordineGiocatori;
+    }
+
+    public int getTurnoAttuale() {
+        return turnoAttuale;
     }
 
 
-
-    public int getTurno_cont() {
-        return turno_cont;
-    }
-
-    public int getTurno() {
-        return turno;
-    }
-
-    public boolean fine(){
+    /*public boolean fine(){
         return this.player.getPuntiVita() == 0 || this.enemy.getPuntiVita() == 0 || this.run;
-    }
+    }*/
 }
