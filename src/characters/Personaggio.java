8904d443d15e1501573a5a08f0851ba958a0426a
AlibabaSakura <rayfirefist_all@makoo.eu>
2018-01-24 20:19:47 +0100

    Small fix on documentation.

--- src/characters/Personaggio.java
+++ src/characters/Personaggio.java
@@ -40,7 +40,7 @@ public abstract class Personaggio {
      * Fa scappare un personaggio dal terreno di gioco:<br>
      * - Calcola la probabilità di riuscire a scappare (50 + (VEL personaggio che si vuole ritirare - VEL nemico))
      * - Genera un numero casuale tra 1 e 100
-     * NB (TODO): Bisognerà prevedere una funzione nella classe di gestione per finire il duello (caso 1) o proseguirlo (caso 0)
+     * Se il numero generato è minore della probabilità, il personaggio riesce a scappare, altrimenti no
      * @param avv Personaggio avversario da cui si sta cercando di scappare
      * @return 1 se il personaggio riesce a scappare; 0 altrimenti
      */
