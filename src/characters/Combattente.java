e0e6bae9b2643cc1cb72e03915850c4e7aa66257
Alessandro Gottardi <alessandrogottardi00@gmail.com>
2018-01-24 11:10:56 +0100

    Soldato: implement IDifesa (See #11)

--- src/characters/Combattente.java
+++ src/characters/Combattente.java
@@ -4,41 +4,18 @@ package ittbuonarroti.rpggame.characters;
  * Classe astratta da cui derivano {@link Soldato} e {@link Mercenario}.
  */
 public abstract class Combattente extends Personaggio implements IAttaccante {
-
-    protected boolean powUp = false; //Danni raddoppiati
-    protected boolean isDef = false; //Mitigazione danni
+    private int moltAttacco = 1; //Mitigazione danni
 
     public Combattente(int puntiVitaTotali, int attacco, int difesa, int velocita, int puntiStaminaTotali, String nome, char sesso) {
         super(puntiVitaTotali, attacco, difesa, velocita, puntiStaminaTotali, nome, sesso);
     }
 
     /**
-     * Moltiplicatore danno (da applicare durante il calcolo dei danni)
-     * @return il valore del danno da moltiplicare
-     */
-    private int moltiplicatoreAttacco() {
-        if (this.powUp)
-            return 2;
-        else
-            return 1;
-    }
-
-    /**
-     * Override di {@link Personaggio#modificaPuntiVita(int)} che tiene conto della modalità difesa
-     */
-    public void modificaPuntiVita(int valore) {
-        if (this.isDef && valore < 0) {
-            // TODO Mitigazione (qui valore verrà modificato)
-        }
-        super.modificaPuntiVita(valore);
-    }
-
-    /**
      * Implementazione di {@link IAttaccante#attacca(Personaggio)}
      */
     // TODO: rivedere implementazione
-    public int attacca(Personaggio nemico) {
-        return -((this.getAttacco() - nemico.getDifesa()) * moltiplicatoreAttacco());
-    }
+    /*public int attacca(Personaggio nemico) {
+        return -((this.getAttacco() - nemico.getDifesa()) * moltAttacco);
+    }*/
 
 }
--- src/characters/Soldato.java
+++ src/characters/Soldato.java
@@ -3,7 +3,8 @@ package ittbuonarroti.rpggame.characters;
 /**
  * Rappresenta un'istanza del personaggio Soldato.
  */
-public class Soldato extends Combattente {
+public class Soldato extends Combattente implements IDifesa {
+    private boolean isDef = false; //Danni raddoppiati
 
     public Soldato(String nome, char sesso) {
         super(180, 55, 35, 20, 25, nome, sesso);
@@ -13,4 +14,23 @@ public class Soldato extends Combattente {
         super(180, 55, 35, 20, 25, "Soldato", 'M');
     }
 
+    /**
+     * Override di {@link Personaggio#modificaPuntiVita(int)} che tiene conto della modalità difesa
+     */
+    public void modificaPuntiVita(int valore) {
+        if (this.isDef && valore < 0) {
+            valore -= valore * 50 / 100;
+            isDef = false;
+        }
+        super.modificaPuntiVita(valore);
+    }
+
+    /**
+     * Il personaggio perde un turno per poi subire danni dimezzati nel turno successivo
+     * TODO: Far perdere un turno
+     */
+    @Override
+    public void preparaDifesa() {
+        isDef = true;
+    }
 }
